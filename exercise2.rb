cd382b12486c123a497e6612ea7bc0f627cf6f14
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-03-22 14:14:43 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+# problem 1
+puts "A good tip is usually 15% of the check so a good tip for a 55 dollar meal would be:\n #{55 * 0.15}"
+# problem 2
+puts "A string " + 5.to_s
+# problem 3
+puts "Solution for 45628 * 7839 equals #{45628*7839}"
+# problem 4
+puts "the answer is true"
